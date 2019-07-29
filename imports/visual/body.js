8e709d3b22e3c81793ff91a8c4ec443f9a2aff88
RebeccaSeraphine <rebecca.s.mueller@web.de>
2019-07-29 12:20:37 +0200

    adding and removing images

--- imports/visual/body.js
+++ imports/visual/body.js
@@ -1,12 +1,18 @@
 import { Template } from 'meteor/templating';
 import { Allimages } from '../logic/imagecollection.js';
 import './body.html';
+import './addimages.html';
 
 Template.body.helpers({
     images() { // images connects to {{#each images}}
-        return Allimages.find({});
+        return Allimages.find({}, { sort: { createdAt: -1 } });
     },
 });
+
+
+//////////////////////////////
+// ADDING IMAGES TO COLLECTION 
+//////////////////////////////
 
 Template.addimages.events({
     'submit .newimage' (event) {
@@ -22,7 +28,20 @@ Template.addimages.events({
         Allimages.insert({
             image_title,
             image_source,
-            image_alt
+            image_alt,
+            createdAt: new Date(),
+            image_rating: 0,
         });
-    }
+
+        // Clear the form
+        target.image_title.value = '';
+        target.image_source.value = '';
+        target.image_alt.value = '';
+    },
 });
+
+Template.body.events({
+    'click .delete' () {
+        Allimages.remove(this._id);
+    }
+})
